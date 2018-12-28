97fc59a4dbdbf116d911e36130e87cb8812e861f
sergej.rupainis@gmail.com <sergej.rupainis@gmail.com>
2018-12-28 11:50:33 +0200

    prefetching comment

--- src/containers/Todos/Loadable.jsx
+++ src/containers/Todos/Loadable.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import loadable from '@loadable/component';
 
+// you may wnat to add prefetching: https://www.smooth-code.com/open-source/loadable-components/docs/prefetching/
 export default loadable(() => import('./Todos'), {
   fallback: <div>Loading...</div>,
 });
