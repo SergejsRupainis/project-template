c73cada17da9ad7945c9d4bcc40b478a42f997a8
sergej.rupainis@gmail.com <sergej.rupainis@gmail.com>
2018-12-28 15:37:25 +0200

    Added styled-component for testing

--- src/containers/Todos/__tests__/index.test.js
+++ src/containers/Todos/__tests__/index.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import styled from 'styled-components';
+import 'jest-styled-components';
 
 import { Todos } from '../index';
 
@@ -19,6 +21,17 @@ describe('<Todos />', () => {
 
     const wrapper = shallow(<Todos {...props} />);
     expect(wrapper.find('h1').text()).toBe(title);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('Check styled button', () => {
+  const Button = styled.button`
+    color: red;
+  `;
+
+  test('it works', () => {
+    const wrapper = shallow(<Button />);
     expect(wrapper).toMatchSnapshot();
   });
 });
