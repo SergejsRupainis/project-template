8554f8b9283fa7bde0eb47417d86738b422200eb
sergej.rupainis@gmail.com <sergej.rupainis@gmail.com>
2019-01-03 16:40:05 +0200

    Added agent base

--- src/agent/base.js
+++ src/agent/base.js
@@ -1,1 +1,34 @@
-export default {};
+import superagent from 'superagent';
+
+import { API_ROOT, RESPONSE_TIME } from '../config';
+
+export const responseBody = res => res.body;
+export const processErrors = error => error;
+
+export const request = {
+  get: url =>
+    superagent
+      .get(`${API_ROOT}${url}`)
+      .timeout(RESPONSE_TIME)
+      .set('Accept', 'application/json')
+      .then(responseBody)
+      .catch(processErrors),
+  del: url =>
+    superagent
+      .del(`${API_ROOT}${url}`)
+      .type('json')
+      .catch(processErrors),
+  put: (url, body) =>
+    superagent
+      .put(`${API_ROOT}${url}`, body)
+      .type('json')
+      .catch(processErrors),
+  post: (url, body) =>
+    superagent
+      .post(`${API_ROOT}${url}`, body)
+      .set('Cache-Control', 'no-cache, no-store')
+      .type('json')
+      .catch(processErrors),
+};
+
+export const encode = encodeURIComponent;
--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,4 @@
 export const SUPPORTED_LOCALES = new Set(['en', 'fi']);
 export const DEFAULT_LOCALE = 'en';
+export const API_ROOT = '/api/';
+export const RESPONSE_TIME = 10 * 1000; // milliseconds
